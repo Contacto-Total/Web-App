9c3d5652fa5e512c668779768aeba01b3157c0fa
ContactoTotalQA <ct_samuel_ch@hotmail.com>
2025-08-21 14:12:27 -0500

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     //baseUrl: 'https://chow-driving-gar.ngrok-free.app/api/'
   //baseUrl: 'http://localhost:8080/api/'
-  baseUrl: 'https://24a0226028e5.ngrok-free.app '
+  baseUrl: 'https://24a0226028e5.ngrok-free.app/'
 };
